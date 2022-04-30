1d824549d0d83cfd84f6fe17d37b78737e234256
StackOverthink <blasch57@gmail.com>
2022-04-30 17:37:46 +0200

    Estimated GLMM_PQL

--- Paper/Auxilliary.R
+++ Paper/Auxilliary.R
@@ -19,3 +19,21 @@ invis.rapply <- function(object, f, classes = "ANY", deflt = NULL,
                          how = c("unlist", "replace", "list"), ...){
                          invisible(rapply(object, f, classes = "ANY", deflt = NULL,
                          how = c("unlist", "replace", "list"), ...))}
+
+# Normal plots
+Dens_norm_plot <- \(data = dat_whale, y){
+  
+  # init. plot
+  ggplot(data, aes(.data[[y]])) +
+
+    # hist
+    geom_histogram(aes(y = ..density..), fill = "cornflowerblue", col = "deepskyblue4") +
+    
+    # fit normal
+    stat_function(fun = dnorm, 
+                  args = list(mean = mean(dat_whale[[y]], na.rm = TRUE), 
+                              sd = sd(dat_whale[[y]], na.rm = TRUE)),
+                  col = "darkblue", lwd = 1, lty = "dashed") +
+    theme_bw() 
+  
+}
